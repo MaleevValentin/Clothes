8e8da9065786ddc5937deab446c9ab0af927d24b
Valentin Maleev <maleyev.valentin@gmail.com>
2017-10-04 00:08:57 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function () {
+	$(".owl-carousel_1").owlCarousel({
+		items: 1,
+		nav: true
+	});
+});
+
+$(function () {
+	$(".owl-carousel_2").owlCarousel({
+		items: 5,
+		nav: true,
+		loop: true
+	});
+});
